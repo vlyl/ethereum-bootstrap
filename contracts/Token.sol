3987a014a9c6648e70a56fe1b52c2db6f62757f2
lylyl <liyunlong518@gmail.com>
2018-09-18 16:51:52 +0800

    Update Token.sol
    handle warnings

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,26 +7,25 @@ contract Token {
     event Issue(address account, uint amount);
     event Transfer(address from, address to, uint amount);
 
-    function Token() {
+    constructor() public {
         issuer = msg.sender;
     }
 
-    function issue(address account, uint amount) {
+    function issue(address account, uint amount) public {
         require(msg.sender == issuer);
         balances[account] += amount;
     }
 
-    function transfer(address to, uint amount) {
+    function transfer(address to, uint amount) public {
         require(balances[msg.sender] >= amount);
 
         balances[msg.sender] -= amount;
         balances[to] += amount;
 
-        Transfer(msg.sender, to, amount);
+        emit Transfer(msg.sender, to, amount);
     }
 
-    function getBalance(address account) constant returns (uint) {
+    function getBalance(address account) public constant returns (uint) {
         return balances[account];
     }
 }
-
